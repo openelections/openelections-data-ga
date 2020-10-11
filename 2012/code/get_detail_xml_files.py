c859ad7e729beca3428ccc38ac6436e91f5ae2a8
ATLJoeReed <atljoereed@gmail.com>
2020-10-11 18:00:07 -0400

    Cleanup, consolidate and standardize 20120821 race results

--- 2012/code/get_detail_xml_files.py
+++ 2012/code/get_detail_xml_files.py
@@ -10,14 +10,15 @@ import helpers
 def get_detail_xml_files(county_url, logger):
     j = clarify.Jurisdiction(url=county_url, level='state')
     subs = j.get_subjurisdictions()
+    num_counties = len(subs)
+    logger.info(f'Found {num_counties} counties to process')
     for s in subs:
         county = s.name
         url = s.report_url('xml')
-        if county in ['Coweta', 'Dooly', 'Lowndes', 'McIntosh', 'Oglethorpe']:
-            if not url:
-                logger.error(f'No detail XML URL found for {county} country')
-            else:
-                helpers.download_detail_xml_file(url, county, logger)
+        if not url:
+            logger.error(f'No detail XML URL found for {county} country')
+        else:
+            helpers.download_detail_xml_file(url, county, logger)
 
 
 if __name__ == '__main__':
@@ -28,6 +29,6 @@ if __name__ == '__main__':
 
     logger = helpers.setup_logger_stdout('get_detail_xml_files')
 
-    county_url = 'https://results.enr.clarityelections.com/GA/40378/95366/en/summary.html' # noqa
+    county_url = 'https://results.enr.clarityelections.com/GA/41629/102692/en/summary.html' # noqa
 
     get_detail_xml_files(county_url, logger)
